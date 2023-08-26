bfa31968b88ffdc55c6f0da4629199928b500d9c
joyounga <joayounga@gmail.com>
2023-08-26 14:10:38 +0900

    refactor: OrderItem Entity 도메인 변경(OrderItem -> Order)(#8)

---
+++ delivery/src/main/java/com/joyuna/delivery/domain/order/OrderItem.java
@@ -0,0 +1,41 @@
+package com.joyuna.delivery.domain.order;
+
+import com.joyuna.delivery.domain.item.Item;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id", updatable = false)
+    private Long id;
+
+    @Column(name = "count", nullable = false)
+    private Integer count;
+
+    @Column(name = "order_price", nullable = false)
+    private Integer orderPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Builder
+    public OrderItem(Integer count, Integer orderPrice, Item item, Order order) {
+        this.count = count;
+        this.orderPrice = orderPrice;
+        this.item = item;
+        this.order = order;
+    }
+}
