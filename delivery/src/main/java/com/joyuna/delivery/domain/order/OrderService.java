b7168587b8a94c8b64b3f867da671645fb6a8b1e
joyounga <joayounga@gmail.com>
2023-11-07 00:14:39 +0900

    refactor: 불필요한 주석 제거(#10)

--- delivery/src/main/java/com/joyuna/delivery/domain/order/OrderService.java
+++ delivery/src/main/java/com/joyuna/delivery/domain/order/OrderService.java
@@ -17,15 +17,8 @@ public class OrderService {
         this.itemService = itemService;
         this.orderItemRepository = orderItemRepository;
     }
-//    public void getItemsTotalPrice(OrderItemAddRequest)
-
 
     public OrderCreateResponse createOrder(OrderCreateRequest orderCreateRequest) {
-        // # 1. 사용자가 주문한 음식(상품) 리스트의 금액을 산출한다.
-        // 1-1. orderCreateRequest엔 상품 가격이 없다. orderItemAddRequest에도 주문한 상품의 아이디만 있을 뿐이다.
-        // 1-2. 상품의 아이디를 통해 주문한 상품의 가격을 가져와야한다.
-        //   현재 이곳에서 진행하기엔 여러번 찔러야 하니 request에서 주문 목록을 그대로 들고 item에 가져가서 값을 산출해오는게 좋을 것 같다.
-        // 1-3. 상품의 가격을 갯수에 맞게 곱해서 총 금액을 구해야한다.
 
         TotalPriceRequest totalPriceRequest = new TotalPriceRequest(orderCreateRequest.getOrderItemList());
 
